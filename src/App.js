9eb736d5244db5c9276df73fb01b080f8b58491c
Yhucee14 <uchechukwu7814@gmail.com>
2024-10-17 01:13:29 -0700

    modified header

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Header from './components/Header';
-import Products from './components/Products';
-import Categories from './components/Categories';
+import Home from './components/Home';
 import './index.css';
-import CartItems from "./components/CartItems";
-import Checkout from './components/Checkout'
-import CheckoutSuccess from './components/CheckoutSuccess';
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <Header />
+       
         <Routes>
-          <Route path="/" element={<Categories/>} />
-          <Route path="/cart" element={<CartItems />} />
-          <Route path="/checkout" element={<CheckoutSuccess />} />
+          <Route path="/" element={<Home/>} />
         </Routes>
       </div>
     </Router>
